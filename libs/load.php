2ea87248b224831e11f0064b520e889b16985441
stark <stark@starks-MacBook-Air.local>
2024-01-02 09:34:17 +0530

    initails the php sturcture updated with missing files in TA-1

--- libs/load.php
+++ libs/load.php
@@ -1,1 +1,13 @@
-<?php ?>
+<?php
+
+//TODO: Implement autoload of class files
+require 'vendor/autoload.php';
+function load_template($name)
+{
+  $currentScript = $_SERVER['SCRIPT_NAME'];
+  $pathParts = explode('/', $currentScript);
+  $projectName = $pathParts[1];
+  // print($projectName);
+  // print($_SERVER['DOCUMENT_ROOT'] . "$projectName" . "_templates/$name.php");
+  include $_SERVER['DOCUMENT_ROOT'] . "/travel-app/". "_templates/$name.php"; //not consistant.
+}
